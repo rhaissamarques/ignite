6ad2912fe62139bb8069062ae0d097b2c1148270
Rhaissa Marques <rhaissa.marques@gmail.com>
2024-07-29 08:42:31 -0300

    feat: adding like function

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -45,6 +45,8 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne)
   }
 
+  const isNewCommentEmpty = newCommentText.length === 0
+
   return (
     <article className={styles.post}>
       <header>
@@ -83,7 +85,12 @@ export function Post({ author, publishedAt, content }) {
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button 
+            type='submit'
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
